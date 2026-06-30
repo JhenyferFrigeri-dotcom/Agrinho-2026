295c2b4a222bc76d54484ce88806ab3512fdc59a
JhenyferFrigeri-dotcom <jhenyfer.frigeri@escola.pr.gov.br>
2026-06-29 22:10:44 -0300

    ..

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-// Trajetória direta dos alimentos
+
 const caminhos = {
-    "Arroz": "Caminho do Arroz: Sai da fazenda sustentável onde foi colhido -> Vai para a fábrica para ser limpo e empacotado -> É levado para o mercado -> Chega na cozinha da sua casa.",
-    "Milho": "Caminho do Milho: É plantado com adubo natural na fazenda -> Vai para a fábrica para virar milho em lata -> É distribuído para os mercados -> Chega na sua mesa.",
-    "Chocolate": "Caminho do Chocolate: O cacau é colhido nas árvores da floresta -> Vai para a fábrica para ser transformado em barra -> Vai para as prateleiras do mercado -> Chega até você."
+    "Arroz": "Caminho do Arroz: O arroz deve ser plantado entre setembro e outubro so podendo ser colhido no ano seguinte. Apos isso o arrozinteoz passa por processos de beneficiamento, como encharcamento, polimento e autoclave, para garantir a qualidade e o valor nutricional. E depois o arroz é embalado e levado ate o supermercado onde as pessoas compram e consumem algo que inicialmente precisou do agro.",
+    "Milho": "Caminho do Milho: O milho deve ser plantado entre setembro e novembro so podendo ser colhido de quatro a seis meses depois. Apos isso o milho passa por processos de beneficiamento, como debulha, limpeza e secagem, para garantir a qualidade e o valor nutricional. E depois o milho é embalado e levado ate o supermercado onde as pessoas compram e consumem algo que inicialmente precisou do agro.",
+    "Chocolate": "O cacau deve ser plantado por meio de mudas so podendo ser colhido de três a cinco anos depois. Apos isso o cacau passa por processos de beneficiamento na fábrica, como torrefação, moagem e refino, para garantir a qualidade e o valor nutricional. E depois o chocolate é embalado e levado ate o supermercado onde as pessoas compram e consumem algo que inicialmente precisou do agro."
 };
 
-// Configura o clique nos botões para exibir a resposta na tela
 document.querySelectorAll(".botao-ver").forEach(function(botao) {
     botao.addEventListener("click", function(clique) {
         let produto = clique.target.getAttribute("data-nome");
@@ -15,8 +14,6 @@ document.querySelectorAll(".botao-ver").forEach(function(botao) {
         document.getElementById("texto-resposta").innerText = caminhos[produto];
     });
 });
-
-// Ativa o Modo Escuro
 document.getElementById("botao-escuro").addEventListener("click", function() {
     document.body.classList.toggle("modo-escuro");
 });
